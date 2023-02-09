cd230bf0a48adc6e29a04895cf9df0529ac4e2d7
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2023-02-09 10:11:47 -0500

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,13 +1,18 @@
 import os
 
 def getINodeType(file_name):
-    pass
+    return os.stat(os.path.join(os.getcwd(),file_name)).st_ino
 
 def listEntries(dict):
     pass
 
 def createFiles(dir_name, file_amount):
-    pass
+    os.chdir(dir_name)
+    fileNum = 0
+    for i in range(file_amount):
+        fileNum+=1
+        fileName = "file_"+str(fileNum)+".xls"
+        open(fileName,"a")
 
 def createSubDirectories(dir_name, subdir_amount):
     pass
@@ -21,5 +26,7 @@ def main():
     os.makedirs(os.path.join(currentDirectory, "Lab3"))
     os.chdir("Lab3")
     print(os.getcwd())
-
+    createFiles(os.getcwd(),5)
+    
+    
 main()
