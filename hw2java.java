f02ba0f64918750edc41a4ed2e6d1cd53f696d55
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2023-02-12 14:21:27 -0500

    Update hw2java.java

--- hw2java.java
+++ hw2java.java
@@ -1,34 +1,44 @@
-import java.util.Arrays;
-
-class Stock implements Comparable {
+import java.util.*;
+public class Stock implements Comparable<Stock> {
     private String symbol;
     private String name;
     private double previousClosingPrice;
     private double currentPrice;
-    private int changePercent;
-    
+    private double changePercent;
+
     public Stock(String symbol,String name,double previousClosingPrice,double currentPrice){
         this.symbol = symbol;
         this.name = name;
         this.previousClosingPrice = previousClosingPrice;
         this.currentPrice = currentPrice;
-        changePercent = (currentPrice - previousClosingPrice)/previousClosingPrice;
+        this.changePercent = ((currentPrice - previousClosingPrice)/previousClosingPrice)*100;
     }
+    @Override
     public String toString(){
-        return "Symbol: " + this.symbol + " Name: " + this.name + " Percent Change: " + this.changePercent;
+        return "Symbol: " + this.symbol + " Name: " + this.name + " -- Percent Change: " + this.changePercent;
     }
-    public int compareTo(Object o){
-        return changePercent - ((Stock)o).changePercent;
+    @Override
+    public int compareTo(Stock o){
+        if (this.changePercent > o.changePercent){
+            return 1;
+        } else if (this.changePercent < o.changePercent) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
-}
-
-public class Main{
-    Stock[] array = new Stock[3];
-    Stock[0] = new Stock("ORCL","Oracle Corporation",34.5,34.35);
-    Stock[1] = new Stock("AAPL","Apple",152.21,151.54);
-    Stock[2] = new Stock("LTHM","Livent",25.01,24.53);
-    Arrays.sort(array);
-    for (Object o:array){
-        System.out.println(o.toString());
+    public static void main(String[] args) {
+        Stock[] tickers = {
+                new Stock("ORCL","Oracle",34.5,34.35),
+                new Stock("LTHM","Livent",24.45,25),
+                new Stock("AAPL","Apple",151.23,150.43),
+                new Stock("TSLA","Tesla",194.58,192.20),
+                new Stock("ECL","Ecolab",146.02,145.12)
+        };
+        Arrays.sort(tickers);
+        System.out.println("Stocks in order:");
+        for (Stock stock:tickers){
+            System.out.println(stock.toString());
+        }
     }
 }
